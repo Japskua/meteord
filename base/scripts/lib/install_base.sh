fa174e69a8101ba0361926e38ecce2df0294b7e6
Janne Parkkila <japskua@gmail.com>
2016-08-12 16:40:22 +0300

    2nd try

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 # Set the timezone here
-tz=Europe/Helsinki
-cp -vf /usr/share/zoneinfo/$tz /etc/localtime
-echo $tz | tee /etc/timezone
+echo "Europe/Helsinki" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
 # Now, install the stuff
 apt-get update -y
 apt-get install -y curl bzip2 build-essential python git ntp
