20811370d15405fe42f134990de72f6b78e56220
Janne Parkkila <japskua@gmail.com>
2016-08-12 16:54:17 +0300

    okay, took away the removal of timezones and locales.

--- base/scripts/lib/cleanup.sh
+++ base/scripts/lib/cleanup.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
 ## Borrowed from: https://github.com/chriswessels/meteor-tupperware
+## and futher edited by japskua
 
 set -e
 # Autoremove any junk
 apt-get autoremove -y
 
-# Clean out docs
-rm -rf /usr/share/doc /usr/share/doc-base /usr/share/man /usr/share/locale /usr/share/zoneinfo
+# Clean out docs, BUT NOT THE LOCALE OR TIMEZONES!
+rm -rf /usr/share/doc /usr/share/doc-base /usr/share/man
 
 # Clean out package management dirs
 rm -rf /var/lib/cache /var/lib/log
