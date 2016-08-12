248f77f833a5d3e37a87942068e4074b41fba304
Janne Parkkila <japskua@gmail.com>
2016-08-12 17:01:43 +0300

    Okay, made many changes, as I was kinda stupid previously.

--- binbuild/scripts/install_binbuild_tools.sh
+++ binbuild/scripts/install_binbuild_tools.sh
@@ -1,4 +1,7 @@
 set -e
+# Set the timezone here
+echo "Europe/Helsinki" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
 # for npm module re-building
 apt-get -y install build-essential libssl-dev git python
 npm install -g node-gyp
