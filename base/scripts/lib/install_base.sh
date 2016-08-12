205b221216893f61c8b6757d7152ef8badc4d92d
Janne Parkkila <japskua@gmail.com>
2016-08-12 15:56:50 +0300

    Now allows setting of timezone

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
+# Set the timezone here
+tz=Europe/Helsinki
+cp -vf /usr/share/zoneinfo/$tz /etc/localtime
+echo $tz | tee /etc/timezone
+# Now, install the stuff
 apt-get update -y
-apt-get install -y curl bzip2 build-essential python git
+apt-get install -y curl bzip2 build-essential python git ntp
